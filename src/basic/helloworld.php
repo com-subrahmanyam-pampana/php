fa1ccd2bcf1f82461620203c96e67f07b977a270
com-subrahmanyam-pampana <130865446+com-subrahmanyam-pampana@users.noreply.github.com>
2024-04-27 23:34:19 +0530

    php added

---
+++ src/basic/helloworld.php
@@ -0,0 +1,14 @@
+<!-- A PHP script can be placed anywhere in the document.
+A PHP script starts with <?php and ends with ?> -->
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+?> 
+
+</body>
+</html>
